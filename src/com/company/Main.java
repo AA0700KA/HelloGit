e6392e3c5334c32a68d6dbff33b3ad8644d6977b
Andrew Bondarenko <andrew11010700@gmail.com>
2021-01-12 20:39:31 +0200

    Add factorial numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int number = 5;
+
+        int fact = 1;
+
+        for (int i = 1; i <= number; i++) {
+            fact *= i;
+        }
+
+        System.out.println(fact);
     }
 
 }
